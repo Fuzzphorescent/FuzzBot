b7a72befa7c0ef8da1fc239135e008a69c272d07
Farzana Shajahan <farzana.oranges@gmail.com>
2020-10-31 13:32:41 +0530

    Added reverse reaction role feature

--- src/FuzzBot.py
+++ src/FuzzBot.py
@@ -1,7 +1,9 @@
 import discord
 import config
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
@@ -12,5 +14,30 @@ async def on_message(message):
     if message.guild:
         if message.content == 'pingi':
             await message.channel.send('pongi')
+
+def get_role_by_name(guild, role_name):
+    for role in guild.roles:
+        if role_name == role.name:
+            return role
+
+@client.event
+async def on_raw_reaction_add(payload):
+    if payload.channel_id == int(config.CHANNEL_ID) and \
+    payload.message_id == int(config.MESSAGE_ID) and \
+    payload.emoji.id == int(config.EMOJI_ID):
+        guild = client.get_guild(int(config.GUILD_ID))
+        member = guild.get_member(payload.user_id)
+        await member.add_roles(get_role_by_name(guild, config.ROLE_1))
+        await member.remove_roles(get_role_by_name(guild, config.ROLE_2))
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    if payload.channel_id == int(config.CHANNEL_ID) and \
+    payload.message_id == int(config.MESSAGE_ID) and \
+    payload.emoji.id == int(config.EMOJI_ID):
+        guild = client.get_guild(int(config.GUILD_ID))
+        member = guild.get_member(payload.user_id)
+        await member.add_roles(get_role_by_name(guild, config.ROLE_2))
+        await member.remove_roles(get_role_by_name(guild, config.ROLE_1))
 
 client.run(config.BOT_TOKEN)
